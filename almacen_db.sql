80c96470b1f57b113db2078e359e4aa374965c6f
InnerChaire <119809312+InnerChaire@users.noreply.github.com>
2022-12-07 23:45:34 -0600

    Add files via upload

--- almacen_db.sql
+++ almacen_db.sql
@@ -1,9 +1,10 @@
-DROP DATABASE almacen_db;
-CREATE DATABASE almacen_db;
+
+
+CREATE DATABASE IF NOT EXISTS almacen_db;
 
 USE almacen_db;
 
-CREATE TABLE productos(
+CREATE TABLE IF NOT EXISTS productos(
 	prod_id INT NOT NULL AUTO_INCREMENT,
 	prod_nombre VARCHAR(30) NOT NULL,
     prod_cantidad INT,
@@ -13,7 +14,7 @@ CREATE TABLE productos(
     PRIMARY KEY (prod_id)
 );
 
-CREATE TABLE usuarios(
+CREATE TABLE IF NOT EXISTS usuarios(
 	usu_id INT NOT NULL AUTO_INCREMENT,
     usu_nombre VARCHAR(30) NOT NULL,
     usu_ape_pat VARCHAR(30) NOT NULL,
@@ -24,7 +25,7 @@ CREATE TABLE usuarios(
     PRIMARY KEY (usu_id)
 );
 
-CREATE TABLE registros(
+CREATE TABLE IF NOT EXISTS registros(
 	reg_id INT NOT NULL AUTO_INCREMENT,
 	reg_fecha_ingreso DATE,
     reg_fecha_egreso DATE,
@@ -86,3 +87,58 @@ INSERT INTO usuarios(usu_nombre, usu_ape_pat, usu_ape_mat, usu_password, usu_pue
 	VALUES ('Almacenista01', 'Pedro', 'Ruiz', 'password01', 'Almacenista');
 INSERT INTO usuarios(usu_nombre, usu_ape_pat, usu_ape_mat, usu_password, usu_puesto) 
 	VALUES ('Desempleado', 'Pedro', 'Ruiz', 'password01', 'Desempleado');
+INSERT INTO productos(prod_nombre,prod_cantidad,prod_tipo)
+	VALUES ('Allure','50','Esencia'),
+			('CK One','30','Esencia'),
+            ('212','22','Esencia'),
+            ('212 Heroes','33','Esencia'),
+            ('CK Everyone','45','Esencia'),
+            ('Channel 5','60','Esencia'),
+            ('Coco Mademosielle','34','Esencia'),
+            ('Thank You Next','46','Esencia'),
+            ('Cloud','27','Esencia'),
+            ('Ferrari Black','13','Esencia'),
+            ('L1212 White','10','Perfume'),
+            ('L1212 Red','4','Perfume'),
+            ('Desire','6','Perfume'),
+            ('Zzegna','7','Perfume'),
+            ('Eros','7','Perfume'),
+            ('Versace','8','Perfume'),
+            ('Toy 2','10','Perfume'),
+            ('Light Blue','3','Perfume'),
+            ('Halloween','2','Perfume'),
+            ('Sauvage Dior','5','Perfume'),
+            ('Hollister Jake','13','Loción'),
+            ('Tommy Hilfiger','14','Loción'),
+            ('360','7','Loción'),
+            ('360 Red','8','Loción'),
+            ('Polo Blue','18','Loción'),
+            ('White Diamond','25','Perfumero'),
+            ('Yellow Diamond','23','Perfumero'),
+            ('Polo Deep Blue','15','Perfumero'),
+            ('Her','17','Perfumero'),
+            ('Dylanblue','18','Perfumero');
+            
+INSERT INTO usuarios(usu_nombre, usu_ape_pat, usu_ape_mat, usu_password, usu_puesto)
+		VALUES ('Rigoberto','Martinez','Olmos','Jefaz0','Gerente'),
+				('Raúl','López','Doriga','080901','Almacenista'),
+                ('Juan','Mendoza','Gonzalez','Juanito1','Almacenista'),
+                ('Fernando','Rodríguez','Contreras','Pu55ydest','Almacenista'),
+                ('Mauricio','Flores','Castillo','Loquito66','Almacenista'),
+                ('Carmelo','Díaz','Benitez','password00','Almacenista'),
+                ('Jorge','Oviedo','Guerra','infinite78','Almacenista'),
+                ('Jesús','Monzon','Donovan','DukiG0D','Almacenista'),
+                ('René','García','Peréz','19962406','Almacenista'),
+                ('Luis','Páramo','Chaire','D0nD0n0van','Almacenista');
+                
+#INSERT INTO registros(reg_fecha_ingreso,reg_fecha_egreso,reg_fecha_modificacion,reg_prod_id,reg_usu_id)
+#		VALUES ('2022-12-04','','','1','2'),
+#				('2019-11-03','','','2','3'),
+ #               ('2019-10-17','','','3','4'),
+  #              ('2019-12-15','','','7','5'),
+   #             ('','2021-06-15','','9','6'),
+    #            ('','2021-08-23','','6','7'),
+     #           ('','2022-01-09','','4','8'),
+      #          ('','','2022-01-15','2','8'),
+       #         ('','','2021-06-23','1','7'),
+        #        ('','','2019-10-26','3','6');
